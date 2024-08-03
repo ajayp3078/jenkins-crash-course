cdaa2afc81d8ea018b6557f0acc42550d181a73c
ajayp3078 <131344687+ajayp3078@users.noreply.github.com>
2024-08-03 08:06:02 +0530

    added student class

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    public Student(){
+        System.out.println("Creating Student Object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi, I am student");
+    }
+    
+}
